5ccf70c46b8b26fa499ee55218b2e39d2e4d65af
Mats Kindahl <mats@kindahl.net>
2022-06-14 08:06:49 +0200

    Support selecting schemes and versions
    Adding support for selecting different schemes using the option `-s`
    when doing benchmarking.
    
    Each scheme is represented as a file with functions that are called to
    perform each step in the benchmarking process. Schemes can have
    different tables for storing the received data.
    
    It also add options to pick a specific version to use when running the
    measurements using the `-v` flag. If no version is picked, the default
    version will be used.
    
    Also add support for connecting to different ports.

--- plot.r
+++ plot.r
@@ -1,9 +1,14 @@
 library('RPostgreSQL')
 library(ggplot2)
 
+DB <- Sys.getenv("PGDATABASE")
+HOST <- Sys.getenv("PGHOST")
+PORT <- Sys.getenv("PGPORT")
+
 pg = dbDriver("PostgreSQL")
-con = dbConnect(pg, dbname="influx", host="localhost", port=5432)
+con = dbConnect(pg, dbname=DB, host=HOST, port=PORT)
 table = dbGetQuery(con, "SELECT * FROM measurements")
+
 ggplot(table, aes(x=as.factor(version), y=count)) + 
     geom_boxplot(fill="slateblue", alpha=0.2) + 
     xlab("version")
--- setup.sql
+++ setup.sql
@@ -1,48 +1,6 @@
--- Set up the tables in the magic schema and create the measurements
--- table and a view to measure the total number of rows inserted.
-CREATE SCHEMA magic;
-
-DROP VIEW IF EXISTS combined;
-DROP TABLE IF EXISTS magic.cpu;
-DROP TABLE IF EXISTS magic.disk;
-DROP TABLE IF EXISTS magic.swap;
-DROP TABLE IF EXISTS magic.diskio;
-
-CREATE TABLE magic.cpu (
-    _time timestamptz,
-    cpu text,
-    host text
-);
-
-CREATE TABLE magic.swap (
-    _time timestamptz,
-    _tags jsonb,
-    _fields jsonb
-);
-
-CREATE TABLE magic.disk (
-    _time timestamptz,
-    _tags jsonb,
-    _fields jsonb
-);
-
-CREATE TABLE magic.diskio (
-    _time timestamptz,
-    _tags jsonb,
-    _fields jsonb
-);
-
-CREATE VIEW combined AS
-    SELECT _time, _tags, _fields FROM magic.cpu
-  UNION ALL
-    SELECT _time, _tags, _fields FROM magic.swap
-  UNION ALL
-    SELECT _time, _tags, _fields FROM magic.disk
-  UNION ALL
-    SELECT _time, _tags, _fields FROM magic.diskio;
-
 CREATE TABLE IF NOT EXISTS measurements (
    time timestamptz,
+   scheme text,
    version text,
    count int,
    total int 
